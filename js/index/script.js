3857ec00f4b95e7bcd7aeb5e96fedd6a25348a7b
Elmer Landaverde <elmer@vt.edu>
2014-11-02 21:36:34 -0500

    Add alert for inactive links

--- js/index/script.js
+++ js/index/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   $('.page-header .title').fitText(.9);
   $('.page-header .subtitle').fitText(1.3);
+  $('a.inactive').click(function (e) {
+    e.preventDefault();
+    swal("Coming soon!");
+  });
 });
