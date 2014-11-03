38b9ba15a981ad4d2ff1c4c85b59d02736203fb0
Elmer Landaverde <elmer@vt.edu>
2014-11-03 10:49:10 -0500

    Re-organize index page content

--- js/index/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.page-header .title').fitText(.9);
-  $('.page-header .subtitle').fitText(1.3);
-  $('a.inactive').click(function (e) {
-    e.preventDefault();
-    swal("Coming soon!");
-  });
-});
