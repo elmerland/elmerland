ad50ddc7100f5b4ed4bff9f75dac0c7cd56a02b8
Elmer Landaverde <elmer@vt.edu>
2015-01-12 00:53:05 -0500

    Implement Jekyll workflow

---
+++ js/header.js
@@ -0,0 +1,4 @@
+$(document).ready(function(e) {
+  $('.site-header h1').fitText(1.0, { minFontSize: '20px', maxFontSize: '100px' });
+  $('.site-header .site-nav').fitText(1.5, { minFontSize: '20px', maxFontSize: '50px' });
+});
